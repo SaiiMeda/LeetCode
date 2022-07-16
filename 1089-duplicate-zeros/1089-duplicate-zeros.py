e28d9f7d5584904141ccebf0e57bd77fec3fe8cc
Sai <36968875+WistfulSai@users.noreply.github.com>
2022-07-16 09:00:13 -0400

    Time: 70 ms (94.90%), Space: 14.7 MB (97.63%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.py
+++ 1089-duplicate-zeros/1089-duplicate-zeros.py
@@ -1,17 +1,12 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
         queue_1 = []
-        for i in range(len(arr)):
-            if queue_1 and arr[i] != 0:
+        for i in range(0,len(arr)):
+            if arr[i] == 0:
+                queue_1.append(0)
+                queue_1.append(0)
+                arr[i] = queue_1.pop(0)
+            else:
                 queue_1.append(arr[i])
                 arr[i] = queue_1.pop(0)
-            elif arr[i] == 0:
-                queue_1.append(0)
-                queue_1.append(0) 
-                arr[i] = queue_1.pop(0)
-                
-                
-                
-                    
-                
-                
+        
